6244999c6cff533033a71cd65c8474a11a319126
Theerdha <sajjasrilatha151@gmail.com>
2022-05-13 15:10:33 +0530

    new commit maven

---
+++ src/main/java/org/theerdha/com/theerdha_first_proj/App.java
@@ -0,0 +1,13 @@
+package org.theerdha.com.theerdha_first_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/theerdha/com/theerdha_first_proj/AppTest.java
@@ -0,0 +1,20 @@
+package org.theerdha.com.theerdha_first_proj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
